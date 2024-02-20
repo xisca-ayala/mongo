d4b67abf4f60947dc42601822d94d39218792ab7
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-02-20 14:41:05 +0100

    dia3

--- retosDia3/notas.js
+++ retosDia3/notas.js
@@ -3,8 +3,6 @@ const Notas = require("./schema");
 
 mongoose.connect('mongodb+srv://xiscaayala:Xismaygi86!@mongonube.e1bi7ij.mongodb.net/Notas');
 
-
-
 const createDocument = () =>{
     let data = {
         date: "2023-12-05",
@@ -29,5 +27,84 @@ const createDocument = () =>{
     })
 }
 
-
 createDocument(); 
+
+const notaMedia =() =>{
+    Notas
+    .aggregate([{$match: { "subject_name": "Javascript"}},
+                {$group:{ _id: null, notaMedia: {$avg: "$mark"}}}])
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+};
+
+const totalAlumnos =() =>{
+    Notas
+    .aggregate([{$group: { "_id":"$student_first_name" }, 
+                "Cantidad": {$sum:1}}])
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+};
+
+const listarAlumnos = () =>{
+    Notas
+    .aggregate([{$group:{"_id":"$student_first_name" },
+                "Cantidad":{$sum:1}},
+            { $project:{"Nombre": "$student_first_name",
+                        "Cantidad":1}}
+
+            ])
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+};
+
+const listarProfesores = () =>{
+    Notas
+    .aggregate([{$unwind:"$teachers"},
+                {$group:{"_id": {teacher_first_name:"$teacher_first_name", 
+                                teacher_last_name:"$teacher_last_name"},
+                         "Cantidad":{$sum:1}}}])
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+notaMedia(); 
+totalAlumnos(); 
+listarAlumnos(); 
+
+
+
+
+
+    
+
+
