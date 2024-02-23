78dd6760160ce143e1036084d413d7fec8a05433
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-02-23 12:40:43 +0100

    notas

--- retosDia3/notas.js
+++ retosDia3/notas.js
@@ -3,16 +3,16 @@ const Notas = require("./schema");
 
 mongoose.connect('mongodb+srv://xiscaayala:Xismaygi86!@mongonube.e1bi7ij.mongodb.net/Notas');
 
-const createDocument = (userData) =>{
-    let document = new Notas(userData); 
-    document.save()
-    .then((data)=> {
-        console.log(data);
-    })
-    .catch((err)=>{
-        console.error(err);
-    })
-}
+// const createDocument = (userData) =>{
+//     let document = new Notas(userData); 
+//     document.save()
+//     .then((data)=> {
+//         console.log(data);
+//     })
+//     .catch((err)=>{
+//         console.error(err);
+//     })
+// }
 
 const notaMedia =(subjectName) =>{
     Notas
@@ -37,29 +37,23 @@ const totalAlumnos =() =>{
     })
 };
 
-// const listarAlumnos = () =>{
-//     Notas
-//     .aggregate([{$group:{"_id":{student_first_name:"$student_first_name", 
-//     student_last_name:"$student_last_name"}},
-//                 "Cantidad":{$sum:1}},
-//             { $project:{"_id": 0,
-//                         "Nombre": "$_id",
-//                         "Cantidad":1}}
-//             ])
-//     .then((result)=>{
-//         console.log(result);
-//     })
-//     .catch((err)=>{
-//         console.error(err);
-//     })
-// };
+const listarAlumnos = () =>{
+    Notas
+    .aggregate([{ $project:{"_id":{student_first_name:"$student_first_name", 
+                            student_last_name:"$student_last_name"}}}])
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+};
 
 const listarProfesores = () =>{
     Notas
     .aggregate([{$unwind:"$teachers"},
-                {$group:{"_id": {teacher_first_name:"$teacher_first_name", 
-                                teacher_last_name:"$teacher_last_name"},
-                         "Cantidad":{$sum:1}}}])
+                {$project:{"_id": {teacher_first_name:"$teachers.teacher_first_name", 
+                                teacher_last_name:"$teachers.teacher_last_name"}}}])
     .then((result)=>{
         console.log(result);
     })
@@ -99,15 +93,10 @@ const asignaturasTop = (notaMedia, limit) =>{
 const numeroProfesores = () =>{
     Notas
     .aggregate([{$unwind:"$teachers"},
-                {$group:{"_id":{asignatura:"$subject_name", 
-                profesor:{
-                    nombre: "$teachers.teacher_first_name",
-                    apellido: "$teachers.teacher_last_name"
-                }},
+                {$group:{"_id":"$subject_name",
                 numeroProfesores: {$sum:1}}}])
     .then((result)=>{
         console.log(result);
-        mongoose.disconnect();
     })
     .catch((err)=>{
         console.error(err);
@@ -115,39 +104,41 @@ const numeroProfesores = () =>{
 };
 
 
-let userData = {
-    date: "2023-12-05",
-    mark:9, 
-    student_first_name: "Laia",
-    student_last_name: "Gili",
-    group_name: "3A",
-    subject_name: "Javascript",
-    teachers: [{
-        teacher_first_name: "Lola",
-        teacher_last_name:"Martí"
-    }]
-};
+// let userData1 = {
+//     date: "2023-12-05",
+//     mark:9, 
+//     student_first_name: "Laia",
+//     student_last_name: "Gili",
+//     group_name: "3A",
+//     subject_name: "Javascript",
+//     teachers: [{
+//         teacher_first_name: "Lola",
+//         teacher_last_name:"Martí"
+//     }]
+// };
 
-let userData2 = {
-    date: "2023-10-05",
-    mark:9, 
-    student_first_name: "Mireia",
-    student_last_name: "Flaquer",
-    group_name: "2A",
-    subject_name: "Maquetació",
-    teachers: [{
-        teacher_first_name: "Lola",
-        teacher_last_name:"Martí"
-    }]
-};
+// let userData2 = {
+//     date: "2023-10-05",
+//     mark:9, 
+//     student_first_name: "Mireia",
+//     student_last_name: "Flaquer",
+//     group_name: "2A",
+//     subject_name: "Maquetació",
+//     teachers: [{
+//         teacher_first_name: "Lola",
+//         teacher_last_name:"Martí"
+//     }]
+// };
 
 
 
-createDocument(userData1); 
-createDocument(userData2);
+// createDocument(userData1); 
+// createDocument(userData2);
+
+
 notaMedia("Javascript"); 
 totalAlumnos(); 
-// listarAlumnos(); 
+listarAlumnos(); 
 listarProfesores(); 
 gruposAlumnos(); 
 asignaturasTop(5,5);
