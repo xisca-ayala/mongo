4bad9d752718c2279646505c7167eb3d2bc9e426
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-02-23 09:52:24 +0100

    parametros entrada

--- retosDia2/Photos/photosModel.js
+++ retosDia2/Photos/photosModel.js
@@ -5,78 +5,102 @@ mongoose.connect('mongodb+srv://xiscaayala:Xismaygi86!@mongonube.e1bi7ij.mongodb
 
  
 
-// const createPhotoDoc = () =>{
-//     let data = {
-//         user: "Maria",
-//         photo: "terrmoto.jpg",
-//         title: "Terremoto",
-//         description: "Terremoto"
-//     };
-//     let photoDocument = new Photos(data); 
-//     photoDocument.save()
-//     .then((data)=> {
-//         console.log(data);
-//         mongoose.disconnect();
-//     })
-//     .catch((err)=>{
-//         console.error(err);
-//     })
-// }
+const createPhotoDoc = (userData) =>{
+    let photoDocument = new Photos(userData); 
+    photoDocument.save()
+    .then((data)=> {
+        console.log(data);
+        mongoose.disconnect();
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+}
 
-// const getPhotoDoc = () => {
-//    Photos.find({user:"Pepe"})
-//    .then((data)=>{
-//         console.log(data);
-//         mongoose.disconnect();
-//    })
-//     .catch((err)=>{
-//         console.error(err);
-//     })
-// }
+const getPhotoDoc = (user) => {
+   Photos.find({user})
+   .then((data)=>{
+        console.log(data);
+        mongoose.disconnect();
+   })
+    .catch((err)=>{
+        console.error(err);
+    })
+}
 
-//  const updatePhotosDesc = ()=> {
-//     Photos.updateOne({user: "Pepe",description: "romantico paisaje"},
-//     {description:"frio paisaje", verified: false})
-//      .then((data)=>{
-//          console.log("Se ha modificado correctamente");
-//          console.log(data);
-//             mongoose.disconnect(); 
-//     })
-//      .catch((err)=>{
-//          console.error(err);
-//      })
-// }
+ const updatePhotosDesc = (user, description, new_description)=> {
+    Photos.updateOne({user,description},
+    {$set:{description:new_description, verified: false}})
+     .then((data)=>{
+         console.log("Se ha modificado correctamente");
+         console.log(data);
+            mongoose.disconnect(); 
+    })
+     .catch((err)=>{
+         console.error(err);
+     })
+}
 
-// const deletePhoto = ()=> {
-//     Photos.deleteOne({user: "Pepa", title: "Gato Andando"})
-//     .then((data)=>{
-//         console.log("Se ha eliminado correctamente la photo");
-//         console.log(data);
-//         mongoose.disconnect(); 
-//     })
-//     .catch((err)=>{
-//         console.error(err);
-//     })
-// }
+const deletePhoto = (user, title)=> {
+    Photos.deleteOne({user, title})
+    .then((data)=>{
+        console.log("Se ha eliminado correctamente la photo");
+        console.log(data);
+        mongoose.disconnect(); 
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+}
 
-// const deletePhotos = ()=> {
-//     Photos.deleteMany({user: "Maria"})
-//     .then((data)=>{
-//         console.log("Se han eliminado correctamente las photos");
-//         console.log(data);
-//         mongoose.disconnect(); 
-//     })
-//     .catch((err)=>{
-//         console.error(err);
-//     })
-// }
+const deletePhotos = (user)=> {
+    Photos.deleteMany({user})
+    .then((data)=>{
+        console.log("Se han eliminado correctamente las photos");
+        console.log(data);
+        mongoose.disconnect(); 
+    })
+    .catch((err)=>{
+        console.error(err);
+    })
+}
 
 
-// createPhotoDoc();
-// getPhotoDoc(); 
-// updatePhotosDesc();
-// deletePhoto(); 
-// deletePhotos(); 
+
+let userData1 = {
+    user: "Maria",
+    photo: "volcà.jpg",
+    title: "Volcà",
+    description: "Volcà"
+};
+let userData2 = {
+    user: "Pep",
+    photo: "llac.jpg",
+    title: "Llac",
+    description: "Llac"
+};
+let userData3 = {
+    user: "Llucia",
+    photo: "tormenta.jpg",
+    title: "Tormenta",
+    description: "Tormenta"
+};
+let userData4 = {
+    user: "Pixeris",
+    photo: "nuvols.jpg",
+    title: "Núvols",
+    description: "Núvols"
+};
+
+createPhotoDoc(userData1);
+createPhotoDoc(userData2);
+createPhotoDoc(userData3);
+createPhotoDoc(userData4);
+
+getPhotoDoc("Llucia"); 
+updatePhotosDesc("Pixeris, Núvols, Núvols negres");
+deletePhoto("Pep", "Llac"); 
+deletePhotos("Maria"); 
 
 
 
